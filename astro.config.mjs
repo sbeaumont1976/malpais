508dd57aca40c57b224da25789c1714540fdbbf9
Sergio Beaumont <sbeaumont@opein.com>
2024-10-17 19:22:16 +0100

    🚀 Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  server: {
+    port: 3000, // Establece el puerto en 3000
+  },
+});
