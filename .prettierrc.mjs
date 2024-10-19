46d653c4cb9ab663d604cfade018035b1d80f473
Sergio Beaumont <sbeaumont@opein.com>
2024-10-19 14:08:17 +0100

    ✨ 8 puntos en el decalogo

---
+++ .prettierrc.mjs
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: false,  // This is how you turn off semicolons, by the way
+  trailingComma: 'all',
+  singleQuote: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  printWidth: 120,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: { parser: 'astro' }
+    },
+  ],
+  overrides: [
+    { 
+      files: ['*.json', '*.md', '*.toml', '*.yml'], 
+      options: { useTabs: false } 
+    }
+  ],
+};
